2f6842ab37703c5b217aa358fb57f1af15749ac3
DarkMarksDoe <darkmarksdoe@gmail.com>
2020-04-25 15:19:32 -0500

    Adding Beauty Comments and VS Code extension

--- python/1.8.Dictionaries-Operations.py
+++ python/1.8.Dictionaries-Operations.py
@@ -1,20 +1,21 @@
-#                               COURSE DATA STRUCTURES
-#
-#               Native Python Data Structures, Operators for Sequence
-#               Types like: Strings, List, Tuples
-#               UDEMY LINK: https://www.udemy.com/course/python-data-structures-a-to-z/
 
-# If this code works, it was written by DarkMarksDoe. If not, I don't know
-# who wrote it, just pass away
+#!                               COURSE DATA STRUCTURES
+
+#*               Native Python Data Structures, Operators for Sequence
+#*               Types like: Strings, List, Tuples
+#?               UDEMY LINK: https://www.udemy.com/course/python-data-structures-a-to-z/
+
+#* If this code works, it was written by DarkMarksDoe. If not, I don't know
+#* who wrote it, just pass away
 
 
-# INFO ABOUT DICTIONARIES
+#TODO INFO ABOUT DICTIONARIES
 
-# ~ Key/Value pairs
-# ~ Associative array, like Java HashMap
-# ~ Dicts are Unordered
+#* Key/Value pairs
+#* Associative array, like Java HashMap
+#* Dicts are Unordered
 
-#                                     CONSTRUCTORS
+#!                                     CONSTRUCTORS
 print('\n')
 x = {
     'pork': 25.3,
@@ -23,7 +24,7 @@ x = {
 }
 print('Normal Map:', x)
 
-# Parse from list of tuples
+#? Parse from list of tuples
 x = dict(
     [
         ('pork', 25.3),
@@ -33,7 +34,7 @@ x = dict(
 )
 print('Parse Map from List of Tuples:', x)
 
-# Parse list of declarations
+#? Parse list of declarations
 x = dict(
     pork=25.3,
     beef=33.8,
@@ -41,25 +42,25 @@ x = dict(
 )
 print('Parse Map from declarations:', x)
 
-#                                 DICT OPERATIONS
+#!                                DICT OPERATIONS
 print('\n')
 
-# Add or Update an item
+#? Add or Update an item
 x['shrimp'] = 38.2
 print('Add:', x)
 
-# Delete an item
+#? Delete an item
 del(x['shrimp'])
 print('Delete:', x)
 
-# Delete all items from dict x
+#? Delete all items from dict x
 x.clear()
 print('Clear:', x)
 
-# Delete dict x
+#? Delete dict x
 del(x)
 
-#                              ACCESING KEYS AND VALUES IN A DICT
+#!                              ACCESING KEYS AND VALUES IN A DICT
 print('\n')
 
 y = {
@@ -67,27 +68,27 @@ y = {
     'beef': 33.8,
     'chicken': 22.7
 }
-# Get  keys
+#? Get  keys
 print('Keys of y:', y.keys())
-# Get  values
+#? Get  values
 print('Values of y:', y.values())
-# Get  items
+#? Get  items
 print('Items of y:', y.items())
 
-# Check Membership in y_keys (only looks in keys, not values)
+#? Check Membership in y_keys (only looks in keys, not values)
 print('Membership Keys:', 'beef' in y)
 
-# Check Membership in y_values
+#? Check Membership in y_values
 print('Membership Values:', 'clams' in y)
 
-#                              ITERATING A DICT
+#!                              ITERATING A DICT
 print('\n')
 
-# Print enumerated key and their values
+#? Print enumerated key and their values
 for index, item in enumerate(y):
     print('key ', index, ':', item, ', value:', y[item])
 
-# Print value and key from y.items
+#? Print value and key from y.items
 for k, v in y.items():
     print(k, v)
 
